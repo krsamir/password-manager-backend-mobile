ff654a45105aed9fd7ea6785660165f5fa54f05b
Samir Kumar <46542096+krsamir@users.noreply.github.com>
2023-05-09 22:54:02 +0530

    adding graphql

---
+++ server/server.js
@@ -0,0 +1,52 @@
+// npm install @apollo/server express graphql cors body-parser
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+
+const app = express();
+const typeDefs = `
+type Book {
+  title: String
+  author: String
+}
+type Query {
+  books: [Book]
+}
+`;
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+const httpServer = http.createServer(app);
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+await server.start();
+app.use(
+  '/graphql',
+  cors(),
+  express.json(),
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ token: req.headers.token }),
+  })
+);
+
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+console.log(`🚀 Server ready at http://localhost:4000`);
+console.log(`🚀 Graphql ready at http://localhost:4000/graphql`);
